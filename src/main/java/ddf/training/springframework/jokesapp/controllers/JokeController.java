164130063125effc5f01914cc3078dbd96a4ce2d
Dany Koudja <koudjadany@gmail.com>
2020-01-22 14:14:43 +0100

    Definition of the thymeleaf view of chucknorris.

--- src/main/java/ddf/training/springframework/jokesapp/controllers/JokeController.java
+++ src/main/java/ddf/training/springframework/jokesapp/controllers/JokeController.java
@@ -3,6 +3,7 @@ package ddf.training.springframework.jokesapp.controllers;
 import ddf.training.springframework.jokesapp.services.JokeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class JokeController {
@@ -13,6 +14,7 @@ public class JokeController {
         this.jokeService = jokeService;
     }
 
+    @GetMapping("/")
     public String getJoke(Model model){
         model.addAttribute("joke", jokeService.getRandomJoke());
         return "chucknorris";
