e0d3549479257a0c7bc113c5379ffe00141e6d25
Dany Koudja <koudjadany@gmail.com>
2020-01-24 11:27:18 +0100

    Adding a Spring Java configuration for adding the external ChuckNorrisQuotes class as a bean into the Spring context. Closes #4

---
+++ src/main/java/ddf/training/springframework/jokesapp/config/ChuckConfig.java
@@ -0,0 +1,14 @@
+package ddf.training.springframework.jokesapp.config;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChuckConfig {
+
+    @Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes(){
+        return new ChuckNorrisQuotes();
+    }
+}
